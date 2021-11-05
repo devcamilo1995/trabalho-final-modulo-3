08fd167888f234c3e264a01d3f5cd2997bafabcb
devcamilo1995 <matheus.camilo1617@gmail.com>
2021-11-05 15:09:00 -0300

    trabalho atualizado2

--- src/main/java/com/dbc/trabalhovemser/dto/UsuarioDTO.java
+++ src/main/java/com/dbc/trabalhovemser/dto/UsuarioDTO.java
@@ -12,7 +12,6 @@ import java.time.LocalDate;
 public class UsuarioDTO {
     private Integer idUsuario;
     private String nome;
-    private String cpf;
     private LocalDate dataNascimento;
     private String email;
     private TipoUsuario tipoUsuario; // 0 - Comum, 1 - Admin.
