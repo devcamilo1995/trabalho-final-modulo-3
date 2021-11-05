e7de286ef6eb01ccfbb3fd8ae20aa6f1fdc8031b
tiagocoelhosilva86 <tiagospaik63@gmail.com>
2021-11-05 16:40:24 -0300

    merge

--- src/main/java/com/dbc/trabalhovemser/dto/ReservaDTO.java
+++ src/main/java/com/dbc/trabalhovemser/dto/ReservaDTO.java
@@ -7,7 +7,6 @@ import com.dbc.trabalhovemser.entity.UsuarioEntity;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
-<<<<<<< HEAD
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
@@ -16,9 +15,6 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-=======
-
->>>>>>> e3158f2c05f5319b52dedf1eb457d6b5a07281e3
 public class ReservaDTO {
     private Integer idReserva;
 
