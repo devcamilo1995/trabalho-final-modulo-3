eee601f97799aafb0209232b9153959f694a1b25
gsilvaro11 <guiuser1999@gmail.com>
2021-11-05 10:39:57 -0300

    Adicionando dtos, service, repository de Hoteis

--- src/main/java/com/dbc/trabalhovemser/controller/HoteisController.java
+++ src/main/java/com/dbc/trabalhovemser/controller/HoteisController.java
@@ -1,4 +1,31 @@
 package com.dbc.trabalhovemser.controller;
 
+import com.dbc.trabalhovemser.dto.HoteisDTO;
+import com.dbc.trabalhovemser.dto.QuartosDTO;
+import com.dbc.trabalhovemser.service.HoteisService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Validated
+@RestController
+@RequestMapping("/hoteis")
+@Slf4j
+@RequiredArgsConstructor
 public class HoteisController {
+    private final HoteisService hoteisService;
+
+//    @ApiOperation(V)
+    @GetMapping
+    public List<HoteisDTO> list(){
+        return hoteisService.list();
+    }
+
+
 }
--- src/main/java/com/dbc/trabalhovemser/entity/HoteisEntity.java
+++ src/main/java/com/dbc/trabalhovemser/entity/HoteisEntity.java
@@ -3,14 +3,17 @@ package com.dbc.trabalhovemser.entity;
 
 import lombok.*;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class HoteisEntity {
     private Integer idHotel;
-    private EnderecoEntity enderecoEntity;
-    private String nome;
+    private String nomeHotel;
+    private String nomeEstado;
+    private String nomecidade;
+    private String cep;
+    private Integer numero;
+    private String logradouro;
+
 }
 
