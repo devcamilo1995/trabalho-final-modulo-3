524fc9c63091c5d19f3fbb0788515737b24c345c
Leo-kardo <leonardo_c_g@hotmail.com>
2021-11-22 10:22:31 -0300

    get hoteis pelo usuário adicionado

--- src/main/java/com/dbc/trabalhovemser/security/WebSecurityConfig.java
+++ src/main/java/com/dbc/trabalhovemser/security/WebSecurityConfig.java
@@ -34,11 +34,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .antMatchers("/").permitAll()
                 .antMatchers("/auth").permitAll()
-                .antMatchers(HttpMethod.GET, "/usuario/{idUsuario}/**").hasRole("USUARIO")
                 .antMatchers(HttpMethod.POST, "/usuario/**", "/reserva/**").hasRole("USUARIO")
                 .antMatchers(HttpMethod.PUT, "/usuario/**", "/reserva/**").hasRole("USUARIO")
                 .antMatchers(HttpMethod.DELETE, "/usuario/**", "/reserva/**").hasRole("USUARIO")
-                .antMatchers(HttpMethod.GET, "/usuario/{idUsuario}/*", "/quartos/*").hasRole("USUARIO")
+                .antMatchers(HttpMethod.GET, "/usuario/{idUsuario}/*", "/quartos/*", "/hoteis/*").hasRole("USUARIO")
                 .antMatchers("/**").hasRole("ADMIN") //ROLE_ADMIN
                 .anyRequest().authenticated()
 
